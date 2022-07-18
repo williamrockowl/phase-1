33294e757052b22b9a7c9c9e7fb4fb971b721f8a
William Saulon <willfrombuffalo13@gmail.com>
2022-07-18 16:45:40 -0400

    Starting code for EventListener to change bg-color onClick

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Eventlistener if button was clicked
-document.querySelector('#alert').addEventListener('click',() => alert('Button was clicked'))
+document
+  .querySelector("#alert")
+  .addEventListener("click", () => alert("Button was clicked"));
+
+// Eventlistener to change Background color
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => (document.body.style.color = "red"));
