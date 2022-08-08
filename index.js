4b52114eb9cbc5936f0bdc73ea403fab6a126566
William Saulon <willfrombuffalo13@gmail.com>
2022-08-07 21:24:37 -0400

    Added Fetch for JSON

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ document
 // Eventlistener to change Background color
 const btn = document.getElementById("btn");
 btn.addEventListener('mouseover', () => (document.body.style.color = "red"));
+
+// Using Fetch to grab Data from API
+fetch("https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json")
+  .then(function (response) {
+    console.log(response);
+    return response.json();
+  })
+  .then (function (data) {
+    console.log(data);
+  });
